c128f26ca36b7eb1214a7f9aae049e0edda03119
mjordan <mjordan@digium.com>
2014-09-13 12:38:19 -0500

    http_client: Update async swagger client; add tests
    This patch updates the async client with some more functionality, and starts
    to add some very basic unit tests.

--- twisted_ari/http_client.py
+++ twisted_ari/http_client.py
@@ -1,3 +1,9 @@
+"""FOO!
+"""
+
+import json
+import urllib
+import logging
 
 from base64 import b64encode
 
@@ -7,70 +13,248 @@ from twisted.internet import reactor
 from twisted.internet.defer import succeed
 from twisted.web.client import Agent, HTTPConnectionPool
 from twisted.web.http_headers import Headers
+from twisted.web.iweb import IBodyProducer
 from swaggerpy.http_client import HttpClient, Authenticator
 
-import json
+from autobahn.twisted.websocket import WebSocketClientFactory, \
+    WebSocketClientProtocol, connectWS
 
-class Request(Object):
+LOGGER = logging.getLogger(__name__)
+
+class Request(object):
+    """A working object for an HTTP or WS request
+
+    Attributes:
+    method
+    url
+    params
+    headers
+    body_producer
+    """
 
     def __init__(self, method, url, params=None):
+        """Constructor
+
+        Args:
+        method
+        url
+        params
+        """
         self.method = method
         self.url = url
         self.params = params or {}
         self.headers = Headers()
         self.body_producer = None
 
+    def build_url(self):
+        """Build the URL from this object
+
+        Returns:
+        A string representation of the URL
+        """
+        if len(self.params) == 0:
+            return self.url
+        encoded_params = urllib.urlencode(self.params)
+        return "%s?%s" % (self.url, encoded_params)
+
 class JSONBodyProducer(object):
+    """
+
+    Attributes:
+    body
+    length
+    """
     implements(IBodyProducer)
 
     def __init__(self, body):
         self.body = json.dumps(body, separators=(',', ':'))
         self.length = len(body)
 
+    #pylint: disable=C0103
     def startProducing(self, consumer):
+        """
+
+        Args:
+        consumer
+        """
         consumer.write(self.body)
         return succeed(None)
 
+    #pylint: disable=C0103,C0111
     def pauseProducing(self):
         pass
 
+    #pylint: disable=C0103,C0111
     def stopProducing(self):
         pass
 
 
 class BasicAuthenticator(Authenticator):
+    """
 
+    Attributes:
+    userpass
+    """
     def __init__(self, host, username, password):
+        """Constructor
+
+        Args:
+        host
+        username
+        password
+        """
         super(BasicAuthenticator, self).__init__(host)
 
         self.userpass = b64encode(b'%s:%s' % (username, password))
 
     def apply(self, request):
+        """
+
+        Args:
+        request
+        """
         request.headers.addRawHeader(name=b'Authorization',
             value=b'Basic ' + self.userpass)
 
 class ApiKeyAuthenticator(Authenticator):
+    """
+
+    Attributes:
+    param_name
+    api_key
+    """
 
     def __init__(self, host, api_key, param_name='api_key'):
+        """Constructor
+
+        Args:
+        host
+        api_key
+        param_name
+        """
         super(ApiKeyAuthenticator, self).__init__(host)
         self.param_name = param_name
         self.api_key = api_key
 
     def apply(self, request):
-        self.request.params[self.param_name] = self.api_key
+        """
+
+        Args:
+        request
+        """
+        request.params[self.param_name] = self.api_key
 
 
-class AsyncHttpClient(HttpClient):
+class ARIWebSocketClientFactory(WebSocketClientFactory):
+    """Twisted WebSocket client protocol factory for ARI
+
+    This is a relatively simple wrapper around WebSocketClientFactory that
+    produces instances of ARIWebSocketClientProtocol.
+
+    Attributes:
+    receiver An instance of ARIEventResource
+    """
+
+    def __init__(self, receiver, url, debug=True):
+        """Constructor
+
+        Args:
+        receiver The instance of ARIEventResource that will receive updates
+        url      The URL to connect the WebSocket to
+        debug    Optional. Enable greater debugging in WebSocketClientFactory
+        """
+        WebSocketClientFactory.__init__(self, url, debug=debug,
+                                        protocols=['ari'])
+        self.receiver = receiver
+
+    #pylint: disable=C0103
+    def buildProtocol(self, addr):
+        """Build an ARIWebSocketClientProtocol instance
+
+        Returns:
+        An instance of ARIWebSocketClientProtocol
+        """
+        return ARIWebSocketClientProtocol(self, self.receiver)
+
+    def connect(self):
+        """Connect the client factory to the WebSocket server
+
+        Returns:
+        An instance of twisted.internet.interfaces.IConnector
+        """
+        return connectWS(self)
+
+#pylint: disable=R0904
+class ARIWebSocketClientProtocol(WebSocketClientProtocol):
+    """Twisted WebSocket client protocol for ARI
+
+    This acts as a very thin facade over WebSocketClientProtocol. Most of the
+    work is deferred to the receiver, for two reasons:
+    (1) It is much easier to unit test something that doesn't inherit from
+        an Autobahn class
+    (2) Our ARIEventResource receiver object maps the received JSON events
+        to what our Asterisk server has told us it wants. That doesn't belong
+        in our protocol.
+
+    Attributes:
+    receiver Our ARIEventResource receiver object
+    """
+
+    def __init__(self, receiver):
+        """Constructor
+
+        Args:
+        receiver Our ARIEventResource receiver object
+        """
+        self.receiver = receiver
+
+    #pylint: disable=C0103
+    def onOpen(self):
+        """Callback called when the WebSocket connection is made"""
+        self.receiver.on_open(self)
+
+    #pylint: disable=C0103
+    def onClose(self, wasClean, code, reason):
+        """Callback called when the WebSocket connection is closed"""
+        self.receiver.on_close(self, wasClean, code, reason)
+
+    #pylint: disable=C0103,W0613
+    def onMessage(self, msg, binary):
+        """Callback called when the WebSocket receives a message"""
+        event = None
+        try:
+            event = json.loads(msg)
+        except ValueError:
+            LOGGER.warn('Received invalid JSON from server: %s' % msg)
+            return
+        self.receiver.on_event(event)
+
+
+class AsyncHTTPClient(HttpClient):
+    """@@@@
+
+    Attributes:
+    http_pool
+    agent
+    authenticator
+    ws_conn
+    """
 
     def __init__(self):
         """Constructor
         """
-        pool = HTTPConnectionPool(reactor)
-        self.agent = Agent(reactor, pool=pool)
+        super(AsyncHTTPClient, self).__init__()
+        self.http_pool = HTTPConnectionPool(reactor)
+        self.agent = Agent(reactor, pool=self.http_pool)
+        self.authenticator = None
+        self.ws_conn = None
 
     def close(self):
-        self.session.close()
-        # There's no WebSocket factory to close; close connections individually
+        """
+        """
+        self.http_pool.closeCachedConnections()
+        if self.ws_conn:
+            self.ws_conn.disconnect()
 
     def set_basic_auth(self, host, username, password):
         """Set up a SwaggerPy basic authenticator
@@ -94,6 +278,15 @@ class AsyncHttpClient(HttpClient):
         self.authenticator = ApiKeyAuthenticator(
             host=host, api_key=api_key, param_name=param_name)
 
+    def apply_authentication(self, req):
+        """Apply authentication to a request
+
+        Args:
+        req  The Request instance to apply authentication to
+        """
+        if self.authenticator and self.authenticator.matches(req.url):
+            self.authenticator.apply(req)
+
     def request(self, method, url, params=None, data=None):
         """Perform an HTTP request
 
@@ -111,39 +304,33 @@ class AsyncHttpClient(HttpClient):
         self.apply_authentication(request)
         if data:
             request.headers.addRawHeader('Content-Type', 'application/json')
-            request.body_producer = JSONBodyProducder(data)
-        df = self.agent.request(request.method,
-                                request.url,
-                                request.headers,
-                                request.body_producer)
-        return df
+            request.body_producer = JSONBodyProducer(data)
+        deferred = self.agent.request(request.method,
+                                      request.build_url(),
+                                      request.headers,
+                                      request.body_producer)
+        return deferred
 
     def ws_connect(self, url, params=None):
         """Websocket-client based implementation.
 
-        :return: WebSocket connection
-        :rtype:  websocket.WebSocket
-        """
-        # Build a prototype request and apply authentication to it
-        proto_req = requests.Request('GET', url, params=params)
-        self.apply_authentication(proto_req)
-        # Prepare the request, so params will be put on the url,
-        # and authenticators can manipulate headers
-        preped_req = proto_req.prepare()
-        # Pull the Authorization header, if needed
-        header = ["%s: %s" % (k, v)
-                  for (k, v) in preped_req.headers.items()
-                  if k == 'Authorization']
-        # Pull the URL, which includes query params
-        url = preped_req.url
-        # Requests version 2.0.0 (at least) will no longer form a URL for us
-        # for ws scheme types, so we do it manually
-        if params:
-            joined_params = "&".join(["%s=%s" % (k, v)
-                                     for (k, v) in params.items()])
-            url += "?%s" % joined_params
-        return websocket.create_connection(url, header=header)
+        Args:
+        url
+        params
 
-    def apply_authentication(self, req):
-        if self.authenticator and self.authenticator.matches(req.url):
-            self.authenticator.apply(req)
+        Note that the connection object returned by this function
+        should generally not be used to close the WebSocket connection.
+        The close method should be used instead, as that will close
+        both the WebSocket as well as the persistent HTTP connection.
+
+        Returns:
+        An instance of twisted.internet.interfaces.IConnector
+        """
+        request = Request('GET', url, params=params)
+        self.apply_authentication(request)
+
+        # @@@@ TODO: Add the receiver
+        ws_factory = ARIWebSocketClientFactory(None, request.build_url())
+        self.ws_conn = ws_factory.connect()
+        return self.ws_conn
+
--- twisted_ari_test/http_client_test.py
+++ twisted_ari_test/http_client_test.py
@@ -9,10 +9,13 @@
 
 import unittest
 
-from twisted_ari.http_client import Request
+from base64 import b64encode
+
+from twisted_ari.http_client import Request, BasicAuthenticator, ApiKeyAuthenticator, \
+    ARIWebSocketClientProtocol
 
 class RequestTest(unittest.TestCase):
-    """
+    """Basic tests for the Request object
     """
 
     def test_http_url(self):
@@ -37,7 +40,77 @@ class RequestTest(unittest.TestCase):
         self.assertEqual({'api_key': 'blah', 'app': 'my_app'}, req.params)
         self.assertEqual('ws://foobar.com:8088/foo?app=my_app&api_key=blah', req.build_url())
 
+class BasicAuthenticatorTest(unittest.TestCase):
+    """Tests for the BasicAuthenticator class
+    """
 
+    def test_constructor(self):
+        auth = BasicAuthenticator('http://localhost', 'foo', 'secret')
+        self.assertEqual(b64encode(b'foo:secret'), auth.userpass)
+
+    def test_apply(self):
+        auth = BasicAuthenticator('http://localhost', 'foo', 'secret')
+        req = Request('GET', 'http://foobar.com:8088/foo')
+        auth.apply(req)
+        header = req.headers.getRawHeaders('Authorization')[0]
+        self.assertEqual('Basic %s' % b64encode(b'foo:secret'), header)
+
+class ApiKeyAuthenticatorTest(unittest.TestCase):
+    """Tests for the ApiKeyAuthenticator class
+    """
+
+    def test_constructor(self):
+        auth = ApiKeyAuthenticator('http://localhost', 'secret')
+        self.assertEqual('api_key', auth.param_name)
+        self.assertEqual('secret', auth.api_key)
+
+        auth = ApiKeyAuthenticator('http://localhost', 'secret', param_name='my_key')
+        self.assertEqual('my_key', auth.param_name)
+        self.assertEqual('secret', auth.api_key)
+
+    def test_apply(self):
+        auth = ApiKeyAuthenticator('http://localhost', 'secret')
+        req = Request('GET', 'http://foobar.com:8088/foo')
+        auth.apply(req)
+        self.assertEqual('secret', req.params.get('api_key'))
+
+class ARIWebSocketClientProtocolTest(unittest.TestCase):
+    """Tests for ARIWebSocketClientProtocol
+    """
+
+    class MockReceiver(object):
+
+        def __init__(self):
+            self.opens = 0
+            self.closes = 0
+            self.events = 0
+            self.last_event = None
+
+        def on_open(self):
+            self.opens += 1
+
+        def on_close(self):
+            self.closes += 1
+
+        def on_event(self, ev):
+            self.events += 1
+            self.last_event = ev
+
+    def test_onMessage_nominal(self):
+        receiver = ARIWebSocketClientProtocolTest.MockReceiver()
+        protocol = ARIWebSocketClientProtocol(receiver)
+        json_msg = '["foo",{"bar":["baz",null,1.0,2]}]'
+        protocol.onMessage(json_msg, None)
+        self.assertEqual(1, receiver.events)
+        self.assertEqual('foo', receiver.last_event[0])
+        self.assertEqual(['baz', None, 1.0, 2], receiver.last_event[1].get('bar'))
+
+    def test_onMessage_off_nominal(self):
+        receiver = ARIWebSocketClientProtocolTest.MockReceiver()
+        protocol = ARIWebSocketClientProtocol(receiver)
+        invalid_json_msg = '["foo",{"bar":0}'
+        protocol.onMessage(invalid_json_msg, None)
+        self.assertEqual(0, receiver.events)
 
 if __name__ == '__main__':
     unittest.main()
